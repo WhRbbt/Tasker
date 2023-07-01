b8c4e8f09bcbccf256fdadbd4a5ff0ea2ccebacf
WhRbbt <96valex@gmail.com>
2023-07-01 22:24:18 +0300

    Create Main.java
    Added Main class and Tasker command line interface

---
+++ src/main/java/tasker/Main.java
@@ -0,0 +1,27 @@
+package tasker;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Tasker");
+
+        while (true) {
+            System.out.println("Enter a command (help - list of commands): ");
+            String command = scanner.nextLine();
+
+            switch (command.toLowerCase()) {
+
+                case "exit" -> {
+                    System.out.println("Exit...");
+                    scanner.close();
+                    System.exit(0);
+                }
+                default -> System.out.println("Invalid command");
+            }
+        }
+    }
+}
+
